df355c8db98588e707792983c6574bf46b70a38c
Mathew Loveless <55035909+ComradGamingMC@users.noreply.github.com>
2022-01-21 01:15:03 -0800

    Create version_checker.lua

---
+++ version_checker.lua
@@ -0,0 +1,49 @@
+-
+-- Branding!
+local label =
+[[ 
+  //
+  ||
+  || 
+  ||                      M SCRIPTS WAS HERE!
+  || 
+  ||]]
+
+-- Returns the current version set in fxmanifest.lua
+function GetCurrentVersion()
+	return GetResourceMetadata( GetCurrentResourceName(), "version" )
+end
+
+-- Grabs the latest version number from the web GitHub
+PerformHttpRequest( "https://comradgamingmc.github.io/version.txt", function( err, text, headers )
+	-- Wait to reduce spam
+	Citizen.Wait( 2000 )
+
+	-- Print the branding!
+	print( label )
+
+	-- Get the current resource version
+	local curVer = GetCurrentVersion()
+
+	print( "  ||    Current version: " .. curVer )
+
+	if ( text ~= nil ) then
+		-- Print latest version
+		print( "  ||    Latest recommended version: " .. text .."\n  ||" )
+
+		-- If the versions are different, print it out
+		if ( text ~= curVer ) then
+			print( "  ||    ^1mScripts version is outdated, visit the FiveM forum post to get the latest version.\n^0  \\\\\n" )
+		else
+			print( "  ||    ^2mScripts is up to date!\n^0  ||\n  \\\\\n" )
+		end
+	else
+		-- In case the version can not be requested, print out an error message
+		print( "  ||    ^1There was an error getting the latest version information.\n^0  ||\n  \\\\\n" )
+	end
+
+	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
+	if ( GetCurrentResourceName() ~= "mScripts" ) then
+		print( "^1ERROR: Resource name is not mScripts, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as mScripts^0\n\n" )
+	end
+end )
