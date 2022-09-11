cef66fcde08b59dddece44a3e3de47ee01627e17
Mathew Loveless <55035909+ComradGamingMC@users.noreply.github.com>
2022-09-10 22:15:18 -0700

    UPDATE 1.6

---
+++ client/docs/features.lua
@@ -0,0 +1,37 @@
+# Current Features
+
+* 3D Messages for /me & /do 
+* AI Spawning Control to have more or less AI Traffic and Peds in your world.
+* AI Workers at different locations around the city to give lively feel.
+* Disable Spawning of Default Cops & Removing getting weapons from there vehicles
+* Disables players having Unpopable tires.
+* Removed the Option for players to reroll there vehicle by just turning there tires | WIll look at adding something todo this
+* Auto Pilot Allows certain vehicles to auto drive themself to destination markers without player control.
+* Better AI, Make it so locals will not just shoot players when they do something near gangs and  other stuff.
+* Added a Zoomable Set of Binoculars to view things from a far by Press "TAB" and storing with "Backspace"
+* Add Customable Blips around the map, Custom ones can be added by following template.
+* Make it so breake lights turn on when you are stopped for any period of time.
+* Added a Fix for Custom vehicles something **NULL** in vMenu/ESX/QBCore | Add your vehciles into cl_CarFix.lua to Fix
+* Added DIfferent Carrying emote Motions so you can drag players around | /carry | /carryu | /piggyback /pb
+* Added Dancing emtions via Commadn [Dancing[0-99]]
+* Reseting a Players look (Blood ext) apon deather
+* Adding Dving Vehicles for players while sitting inside vehicles (This is a vMenu thing Remove for ESX/QBCore)
+* Discord Rich Presence so players can much easily join off there friends and join your discord much easier.
+* Added a Limit to how fast a player can drive in a vehicle to shoot a gun out of the windows
+* FingerPointing Allowing players to point in-game to show where someone went 
+* Handsup Scripting By Press "X" Players can put there hands up without having to type a command
+* Hiding in Rubbish bins, Allowing players to hide in big trash cans when fleeing form someone or something
+* Flashing Drive lights when players honk there horn
+* Cleaning up players hud, So there not tons of stuff showing and in there way.
+* When a player gets hurt, Auto change there walk to a injuried style walk/limp
+* Vehicle Interior Lights allows LEOs & Civs to turn on the Dome light in the vehicle by doing "/il" or Press [Z]
+* No Weaponized Vehicles This disables people from using weapons on weaponized vehicles but they can still be droven
+* Plate Randomizer. Randomizes all Local AI Plates and any vehicle that spawns
+* Custom Pause Menu Allows you to change what your pause menu Looks like
+* Hand on Radio, Allows cops to hit a key and put there hand onto there radio to a to RP Quility
+* Ragdoll Allows players to Ragdoll by Press [~] in-game
+* Seatshuffling Light weight code to stop auto seatshuffle to move seats do /shuff 
+* Cruise Control allows players to press [B] and set a max speed for themself.
+* vSync Syncs in-game weather with everyone one the server also allows changing of weather via command
+* Tazer Effects When a player is tazed makes there screen shake and change colors a little bit for a small period of time
+* Watermark adds a constant water make with your server name and Discord link to top right of screen.
